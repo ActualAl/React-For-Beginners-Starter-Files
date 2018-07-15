4d10dc1a0e48b67ad093cfcd53534bcd63793dc3
Alex Hardman <alexhardman@Alexs-MacBook-Pro-2.local>
2018-07-15 10:19:22 +0100

    End of Ch 19 - syncs to Firebase
    added base.js to gitignore

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,83 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
+import base from '../base';
+
 
 class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    componentDidMount() {
+        const { params } = this.props.match;
+        const localStorageRef = localStorage.getItem(params.storeid);
+        console.log(localStorageRef);
+        if (localStorageRef) {
+            this.setState({
+                order: JSON.parse(localStorageRef)
+            });
+        }
+        this.ref = base.syncState(`${params.storeid}/fishes`, {
+            context: this,
+            state: 'fishes'
+        });
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref);
+    }
+
+    componentDidUpdate() {
+        const { params } = this.props.match;
+        localStorage.setItem('red', 'blue');
+        localStorage.setItem(params.storeid, JSON.stringify(this.state.order));
+    }
+
+    addFish = fish => {
+        // 1) copy existing state
+        const fishes = { ...this.state.fishes };
+        // 2) add our new fish to the copy
+        fishes[`fish${Date.now()}`] = fish;
+        // 3) set the fishes object to statue
+        this.setState({ fishes });
+
+    };
+
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    }
+
+    addToOrder = key => {
+        // copy existing state
+        const order = { ...this.state.order };
+        // Add or update order
+        order[key] = order[key] + 1 || 1;
+        // setState with order
+        this.setState({ order });
+    }
 
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Catch of the day" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key =>
+                            <Fish
+                                key={key}
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder} />
+                        )}
+                    </ul>
                 </div>
-                <Order />
-                <Inventory />
+                <Order fishes={this.state.fishes} order={this.state.order} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
     }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
 
-    //myInput = React.createRef();
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        event.preventDefault();
+        const storeName = this.myInput.current.value;
+        this.props.history.push(`/store/${storeName}`)
+    }
 
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="store name" name="" id="" />
+                <input
+                    type="text"
+                    required
+                    placeholder="store name"
+                    name=""
+                    id=""
+                    ref={this.myInput}
+                    defaultValue={getFunName()} />
                 <button type="submit">Visit store &rarr;</button>
             </form>
         );
