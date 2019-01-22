7408036a0c29dd0453cfc521d7406b44283c6894
Alex Hardman <alexhardman@Alexs-MacBook-Pro-2.local>
2019-01-22 07:32:05 +0000

    Finished tutorial

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import AddFishForm from './AddFishForm';
+import React from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
-    render() {
-        return (
-            <div className="inventory">
-                <h2>Inventory</h2>
-                <AddFishForm addFish={this.props.addFish} />
-                <button onClick={this.props.loadSampleFishes}>Load sample fishes</button>
-            </div>
-        );
-    }
+  handleChange = (e, key) => {
+    const fish = this.props.fishes[key];
+    console.log(fish);
+  };
 
+  renderInventory = key => {
+    const fish = this.props.fishes[key];
+    return (
+      <div className="fish-edit" key={key}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Fish name"
+          value={fish.name}
+          onChange={e => this.handleChange(e, key)}
+        />
+        <input
+          type="text"
+          name="price"
+          placeholder="Fish price"
+          value={fish.price}
+        />
+        <select name="status" placeholder="Fish status" value={fish.status}>
+          <option value="available">Fresh</option>
+          <option value="unavailable">Sold out</option>
+        </select>
+        <textarea name="desc" placeholder="Fish desc" value={fish.desc} />
+        <input
+          type="text"
+          name="image"
+          placeholder="Fish image"
+          value={fish.image}
+        />
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>
+          Load sample fishes
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Inventory;
